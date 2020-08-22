b7005d415e8fc87ae2309565df72c7a18bea0bd4
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2020-08-21 18:00:24 -0700

    cleaning up the footer page to remove white space

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { HomeComponent } from './home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PdfViewerModule } from 'ng2-pdf-viewer';
 
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -19,7 +20,7 @@ import { PdfViewerModule } from 'ng2-pdf-viewer';
     FooterComponent,
     AboutComponent,
     ContactComponent,
-    HomeComponent
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
+  
 })
 export class HomeComponent implements OnInit {
 
@@ -14,7 +16,7 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  onClick() {
+  showModal() {
     console.log('testing');
   }
 }
