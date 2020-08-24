cff3419f4520aa6ef6f3eef6437fed79834c5e2e
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2020-08-24 16:30:38 -0700

    adding in a better home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,25 +10,24 @@ import {
   styleUrls: ['./home.component.scss'],
   animations: [
     fadeInOnEnterAnimation(),
-    fadeInUpOnEnterAnimation({ anchor: 'enter', duration: 1000, delay: 1500, translate: '30px' })
+    fadeInUpOnEnterAnimation({ anchor: 'enter', duration: 1000, delay: 500, translate: '30px' }),
+    fadeInUpOnEnterAnimation({ anchor: 'enter2', duration: 1000, delay: 2000, translate: '30px' })
+]})
 
-  ]
-  
-})
 export class HomeComponent implements OnInit {
 
   modal = false;
+  modal2 = false;
 
   constructor() {
-    
   }
 
   ngOnInit(): void {
     this.modal = true;
+    this.modal2 = true;
   }
 
   showModal() {
     console.log('testing');
-    //this.modal = true;
   }
 }
